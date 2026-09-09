e2e5c0ae691c9da70aa7dcee630399d86e891784
miazmaa <alc032731@gmail.com>
2026-09-09 18:53:54 +0000

    commented out translate and lookAt

--- boxes.js
+++ boxes.js
@@ -189,7 +189,7 @@ function render() {
 	var at =  vec3(0.0, 0.0, 0.0);
 	var up =  vec3(0.0, 1.0, 0.0);
 
-	modelViewMatrix = translate(0, 0, -10);       //lookAt(eye,at,up);
+	modelViewMatrix = //translate(0, 0, -10);       //lookAt(eye,at,up);
     gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
 	
 	gl.drawArrays(shapes.axes.type, shapes.axes.start, shapes.axes.size);	
